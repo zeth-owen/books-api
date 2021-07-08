dc8579b44074e54e3c1b0112f7a496b1794f1783
Jerrica Bobadilla <jerrica@Jerricas-iMac-13.home>
2021-07-08 13:38:08 -0700

    add mongoose

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
