d90681c16ce2aadb7a1ed3b9e30530ef0cc9a665
Zeth Thomas <zeththomas89@gmail.com>
2024-02-28 17:56:46 -0500

    Setup books api

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended: true}))
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to the Hello World! API')
+  res.send('Welcome to the Books API!')
 })
 
 // LISTEN
